e3976a3f575f407f6d71eb1b5eca126dfedde7f6
Petros Shumie <shumie.peter@gmail.com>
2019-11-25 18:54:36 -0500

    added get request to return data in json format for friends.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,4 +1,13 @@
 // Linking the routes to a series of data sources
 // the data sources hold arrays of information on friend data
 
-// 
+var friendData = require("../app/friends");
+
+// ROUTING
+
+module.exports = function(app) {
+    // API GET REQUEST RETURN DATA IN JSON
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+}
