11f9e9fd0fec2740c0ad87db6eb6c2650a3d51af
Petros Shumie <shumie.peter@gmail.com>
2019-11-25 15:00:15 -0500

    added dependencies for node functionality, also expressed  to node that server will be initialized via express.

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+// Series of NPM packagges that we wll use for server functionality
+var express = require("express");
+var path = require(path);
+
+// Tells node this is an express server
+var app = express();
+
