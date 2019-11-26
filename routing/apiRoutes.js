5b002fdf29b9e510272b463207be9773e40d2593
Petros Shumie <shumie.peter@gmail.com>
2019-11-25 19:10:54 -0500

    created for loop for current friend score to compare scores of current friend to all user scores. The used math.abs to determine the absoulte total once scores groups are tallied.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -6,35 +6,49 @@ var friendData = require("../app/friends");
 // ROUTING
 
 module.exports = function(app) {
-    // API GET REQUEST RETURN DATA IN JSON
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-    });
+  // API GET REQUEST RETURN DATA IN JSON
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
+  // API POST REQUEST FOR DATA
 
-    // API POST REQUEST FOR DATA 
+  app.post("/api/friends", function(req, res) {
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
 
-    app.post("/api/friends", function(req, res) {
+    //Create variables to store user data and iterate through friend list to compare current friend scores
+    var userData = req.body;
+    var userScores = userData.scores;
 
-        var bestMatch = {
-            name: "",
-            photo: "",
-            friendDifference: Infinity
-        };
+    var totalDifference = 0;
 
-        //
-        var userData = req.body;
-        var userScores = userData.scores;
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      totalDifference = 0;
 
-        var totalDifference = 0;
+      console.log(currentFriend.name);
 
-        for (var i = 0; i < friends.length; i++) {
-            var currentFriend = friends[i];
-            totalDifference = 0;
+      //
+      for (var p = 0; p < currentFriend.scores.length; p++) {
+        var currentFriendScore = currentFriend.scores[p];
+        var currentUserScore = userScores[p];
 
-            console.log(currentFriend.name);
+        totalDifference += Math.abs(
+          parseInt(currentUserScore) - parseInt(currentFriendScore)
+        );
+      }
 
-            
-        }
-    })
-}
+      if (totalDifference <= bestMatch.friendsDifference) {
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+
+    
+  });
+};
