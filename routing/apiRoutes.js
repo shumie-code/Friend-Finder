3f660c9f3bd5ef576bdd8086710316607ee9319a
Petros Shumie <shumie.peter@gmail.com>
2019-11-29 17:16:06 -0500

    updated html and api routes for proper pathing

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,7 +1,7 @@
 // Linking the routes to a series of data sources
 // the data sources hold arrays of information on friend data
 
-var friendData = require("../app/friends.js");
+var friends = require("../app/friends.js");
 
 // ROUTING
 
@@ -14,23 +14,80 @@ module.exports = function(app) {
   // API POST REQUEST FOR DATA
 // Infinity for no max difference
   app.post("/api/friends", function(req, res) {
-    var bestMatch = {
-      name: "",
-      photo: "",
-      friendDifference: Infinity
-    };
-
-    //Create variables to store user data and iterate through friend list to compare current friend scores
-    var userData = req.body;
-    var userScores = userData.scores;
-
+    
+    //Comparing users for best match
     var totalDifference = 0;
 
-    for (var i = 0; i < friends.length; i++) {
-      var currentFriend = friends[i];
-      totalDifference = 0;
+    //OBJECT for best match parametershttps://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiDsYGM843mAhUsWN8KHTfRBoMQjRx6BAgBEAQ&url=https%3A%2F%2Fscreenrant.com%2Ftrailer-park-boys-main-characters-details%2F&psig=AOvVaw3tfCGMuxSD62BwSahjjWcK&ust=1575064608731684
+var bestMatch = {
+  name: "",
+  photo: "",
+  friendDifference: Infinity
+};
 
-      console.log(currentFriend.name);
+//Take user survey post and parse data
+   var userData = req.body;
+   var userName = userData.name;
+   var userScores = userData.scores;
+
+   //convert user score to a number
+   var b = userScores.map(function(item) {
+     return parseInt(item, 10);
+   });
+
+   //Object for user data
+   userData = {
+     "name": req.body.name,
+     "photo": req.body.photo,
+     "scores": b
+   };
+
+   console.log("Name: " + userName);
+   console.log("User Score " + userScores);
+
+   // convert the user scores to a number by adding all numbers in array
+   var sum = b.reduce((a, b) => a + b, 0);
+   console.log("Sum of users score " + sum);
+   console.log("Best match friend diff " + bestMatch.friendDifference);
+
+
+   console.log("-----------------------------");
+
+   // Iterate through for loop to consider all friend possibilities from database.
+   for (var i = 0; i < friends.length; i++) {
+     totalDifference = 0;
+     console.log(friends[i].name);
+     console.log("Best match friend diff " + bestMatch.friendDifference);
+
+     var bfriendScore = friends[i].scores.reduce((a, b) => a + b, 0); 
+     console.log("Total friend score " + bfriendScore);
+     totalDifference += Math.abs(sum - bfriendScore);
+     console.log("-----------> " totalDifference);
+
+     // If the sum of differences is less then the differences of the current "best match"
+     // We are looking for the smallest difference (most similar) peoples interests
+     if (totalDifference <= bestMatch.friendDifference) {
+       // Reset the bestMatch to the new friend
+       bestMatch.name = friends[i].name;
+       bestMatch.photo = friends[i].photo;
+       bestMatch.friendDifference = totalDifference;
+       // If someone runs the application a second time they wont get themselves 
+       if (bestMatch.name == userName) {
+         bestMatch.name = "Phil Collins";
+         bestMatch.photo = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiDsYGM843mAhUsWN8KHTfRBoMQjRx6BAgBEAQ&url=https%3A%2F%2Fscreenrant.com%2Ftrailer-park-boys-main-characters-details%2F&psig=AOvVaw3tfCGMuxSD62BwSahjjWcK&ust=1575064608731684";
+         bestMatch.friendDifference = 10;
+       }
+     }
+     console.log(totalDifference + " Total Difference");
+
+   }
+   console.log(bestMatch);
+
+  // Finally save the users data to the database (this has to happen after the check.)
+   friends.push(userData);
+   console.log("New user added");
+   console.log(userData);
+   //Return JSON
 
       //Use for loop to iterate through current friend score list and store in currentFriendScore variable
       for (var p = 0; p < currentFriend.scores.length; p++) {
