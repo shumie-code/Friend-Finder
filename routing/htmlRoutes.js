a87aa72441f05309c2db302a70f66e9bc2f4053a
Petros Shumie <shumie.peter@gmail.com>
2019-12-01 01:55:42 -0500

    minor changes tohtmlROutes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -8,11 +8,11 @@ module.exports = function(app) {
     //Html GET Requests
 
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     // If no matching route is found default to none
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "/../public/home.html"))
+        res.sendFile(path.join(__dirname, "../public/home.html"))
     });
 }
